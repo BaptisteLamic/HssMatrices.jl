ab9d49e425bb76ef9a94ae845614be1f4eaaf0a5
Baptiste Lamic <baptiste.lamic@gmail.com>
2025-01-10 08:29:30 +0100

    Recursion optional for ldiv

--- src/ulvdivide.jl
+++ src/ulvdivide.jl
@@ -10,29 +10,29 @@
 # Written by Boris Bonev, Feb. 2021
 
 # function for acess that imitate the behavior in LinearAlgebra.jl
-ldiv!(hssA::HssMatrix, hssB::HssMatrix) = _ldiv!(copy(hssA), hssB)
+ldiv!(hssA::HssMatrix, hssB::HssMatrix, multithreaded::Bool=HssOptions().multithreaded) = _ldiv!(copy(hssA), hssB, multithreaded)
 # lazy implementation of rdiv!
-function rdiv!(hssA::HssMatrix, hssB::HssMatrix)
-  hssA = adjoint(_ldiv!(adjoint(hssB), adjoint(hssA)))
+function rdiv!(hssA::HssMatrix, hssB::HssMatrix, multithreaded::Bool=HssOptions().multithreaded)
+  hssA = adjoint(_ldiv!(adjoint(hssB), adjoint(hssA), multithreaded))
 end
 
 ## ULV divide algorithm to apply the inverse to another HSS matrix
 # temporary name for function that actually just computes the ULV factorization
 # the cluster structure in hssA and hssB should be compatible w/e that means...
-function _ldiv!(hssA::HssMatrix, hssB::HssMatrix)
+function _ldiv!(hssA::HssMatrix, hssB::HssMatrix, multithreaded::Bool)
   if isleaf(hssA)
     D, U, V = _hssleaf(hssB)
     D = full(hssA) \ D
     return HssMatrix(D, U, V, isroot(hssB))
   else
     # bottom-up stage of the ULV solution algorithm
-    hssL, QU, QL, QV, mk, nk, ktree  = _ulvfactor_leaves!(hssA, 0)
-    hssB = _utransforms!(hssB, QU)
+    hssL, QU, QL, QV, mk, nk, ktree  = _ulvfactor_leaves!(hssA, 0, multithreaded)
+    hssB = _utransforms!(hssB, QU, multithreaded)
     hssQB = _extract_crows(hssB, nk)
-    hssY0 = _ltransforms!(hssB, QL)
+    hssY0 = _ltransforms!(hssB, QL, multithreaded)
 
     # early exit if all remaining blocks are 0.
-    if size(hssQB,1) == 0; return hssB = _vtransforms!(hssB, QV); end
+    if size(hssQB,1) == 0; return hssB = _vtransforms!(hssB, QV, multithreaded); end
     # TODO: this still fails if only one hss block gets fully eliminated - fix that!
 
     hssQB = hssQB - hssL * hssY0 # multiply triangularized part with solved part and substract from the
@@ -44,11 +44,11 @@ function _ldiv!(hssA::HssMatrix, hssB::HssMatrix)
     hssL = prune_leaves!(hssL)
 
     # recursively call mldivide
-    hssY1 = _ldiv!(hssL, hssQB)
+    hssY1 = _ldiv!(hssL, hssQB, multithreaded)
 
     # do the unpacking
     hssB = _unpackadd_rows!(hssY0, hssY1, ktree)
-    hssB = _vtransforms!(hssB, QV)
+    hssB = _vtransforms!(hssB, QV, multithreaded)
     hssB = recompress!(hssB)
   end
 end
@@ -85,7 +85,7 @@ function _ulvreduce!(D::Matrix{T}, U::Matrix{T}, V::Matrix{T}) where T
 end
 
 # recursive function that operates only on the leaves
-function _ulvfactor_leaves!(hssA::HssMatrix{T}, co::Int) where T
+function _ulvfactor_leaves!(hssA::HssMatrix{T}, co::Int, multithreaded::Bool) where T
   if isleaf(hssA)
     cols = collect(co .+ (1:size(hssA,2)))
     m, n = size(hssA.D)
@@ -97,8 +97,8 @@ function _ulvfactor_leaves!(hssA::HssMatrix{T}, co::Int) where T
     return HssMatrix(D, U, V, hssA.rootnode), QU, QL, QV, BinaryNode(mk), BinaryNode(nk), BinaryNode(k)
   else
     m1, n1 = hssA.sz1; m2, n2 = hssA.sz2
-    task = RecursionTools.spawn(_ulvfactor_leaves!,(hssA.A11, co), true)
-    hssA.A22, QU2, QL2, QV2, mk2, nk2, k2 = _ulvfactor_leaves!(hssA.A22, co+n1)
+    task = RecursionTools.spawn(_ulvfactor_leaves!,(hssA.A11, co, multithreaded), multithreaded)
+    hssA.A22, QU2, QL2, QV2, mk2, nk2, k2 = _ulvfactor_leaves!(hssA.A22, co+n1, multithreaded)
     hssA.A11, QU1, QL1, QV1, mk1, nk1, k1 = RecursionTools.fetch(task)
     # update sizes
     hssA.sz1 = size(hssA.A11); hssA.sz2 = size(hssA.A22)
@@ -145,12 +145,12 @@ end
 # generate branch level recursive function for each transform
 for (f,g) in zip((:_utransforms!, :_ltransforms!, :_vtransforms!), (:_utransforms_kernel!, :_ltransforms_kernel!, :_vtransforms_kernel!))
   @eval begin
-    function $f(hssA::HssMatrix, Qtree::BinaryNode)
+    function $f(hssA::HssMatrix, Qtree::BinaryNode, multithreaded::Bool)
       if isleaf(hssA)
         hssA = $g(hssA, Qtree)
       else
-        task = RecursionTools.spawn($f,(hssA.A11, Qtree.left), true)
-        hssA.A22 = $f(hssA.A22, Qtree.right)
+        task = RecursionTools.spawn($f,(hssA.A11, Qtree.left,multithreaded), multithreaded)
+        hssA.A22 = $f(hssA.A22, Qtree.right,multithreaded)
         hssA.A11 = fetch(task)
         hssA.sz1 = size(hssA.A11); hssA.sz2 = size(hssA.A22)
         return hssA
